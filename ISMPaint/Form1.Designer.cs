590871dd503b384a28a14c35d0603b3e889f41c9
Andrey Anchis <andreyanchis@gmail.com>
2019-11-16 18:03:17 +0200

    Работает все, кроме одного случая для прямоугольника

---
+++ ISMPaint/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace ISMPaint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxShapes = new System.Windows.Forms.ListBox();
+            this.buttonDot = new System.Windows.Forms.Button();
+            this.buttonLine = new System.Windows.Forms.Button();
+            this.buttonCircle = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonColor = new System.Windows.Forms.Button();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.buttonTest = new System.Windows.Forms.Button();
+            this.pictureBoxMain = new System.Windows.Forms.PictureBox();
+            this.buttonEllipse = new System.Windows.Forms.Button();
+            this.buttonRectangle = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBoxShapes
+            // 
+            this.listBoxShapes.FormattingEnabled = true;
+            this.listBoxShapes.ItemHeight = 20;
+            this.listBoxShapes.Location = new System.Drawing.Point(12, 60);
+            this.listBoxShapes.Name = "listBoxShapes";
+            this.listBoxShapes.Size = new System.Drawing.Size(151, 344);
+            this.listBoxShapes.TabIndex = 0;
+            // 
+            // buttonDot
+            // 
+            this.buttonDot.Location = new System.Drawing.Point(127, 12);
+            this.buttonDot.Name = "buttonDot";
+            this.buttonDot.Size = new System.Drawing.Size(53, 31);
+            this.buttonDot.TabIndex = 1;
+            this.buttonDot.Text = "Dot";
+            this.buttonDot.UseVisualStyleBackColor = true;
+            this.buttonDot.Click += new System.EventHandler(this.ButtonDot_Click);
+            // 
+            // buttonLine
+            // 
+            this.buttonLine.Location = new System.Drawing.Point(186, 12);
+            this.buttonLine.Name = "buttonLine";
+            this.buttonLine.Size = new System.Drawing.Size(59, 31);
+            this.buttonLine.TabIndex = 2;
+            this.buttonLine.Text = "Line";
+            this.buttonLine.UseVisualStyleBackColor = true;
+            this.buttonLine.Click += new System.EventHandler(this.ButtonLine_Click);
+            // 
+            // buttonCircle
+            // 
+            this.buttonCircle.Location = new System.Drawing.Point(251, 12);
+            this.buttonCircle.Name = "buttonCircle";
+            this.buttonCircle.Size = new System.Drawing.Size(62, 31);
+            this.buttonCircle.TabIndex = 3;
+            this.buttonCircle.Text = "Circle";
+            this.buttonCircle.UseVisualStyleBackColor = true;
+            this.buttonCircle.Click += new System.EventHandler(this.ButtonCircle_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(37, 415);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(92, 32);
+            this.buttonDelete.TabIndex = 4;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.ButtonDelete_Click);
+            // 
+            // buttonColor
+            // 
+            this.buttonColor.Location = new System.Drawing.Point(12, 16);
+            this.buttonColor.Name = "buttonColor";
+            this.buttonColor.Size = new System.Drawing.Size(33, 27);
+            this.buttonColor.TabIndex = 5;
+            this.buttonColor.UseVisualStyleBackColor = true;
+            this.buttonColor.Click += new System.EventHandler(this.ButtonColor_Click);
+            // 
+            // buttonTest
+            // 
+            this.buttonTest.Location = new System.Drawing.Point(158, 415);
+            this.buttonTest.Name = "buttonTest";
+            this.buttonTest.Size = new System.Drawing.Size(75, 23);
+            this.buttonTest.TabIndex = 6;
+            this.buttonTest.Text = "test";
+            this.buttonTest.UseVisualStyleBackColor = true;
+            this.buttonTest.Click += new System.EventHandler(this.ButtonTest_Click);
+            // 
+            // pictureBoxMain
+            // 
+            this.pictureBoxMain.Location = new System.Drawing.Point(169, 60);
+            this.pictureBoxMain.Name = "pictureBoxMain";
+            this.pictureBoxMain.Size = new System.Drawing.Size(590, 344);
+            this.pictureBoxMain.TabIndex = 7;
+            this.pictureBoxMain.TabStop = false;
+            this.pictureBoxMain.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBoxMain_Paint);
+            this.pictureBoxMain.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PictureBoxMain_MouseDown);
+            this.pictureBoxMain.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PictureBoxMain_MouseMove);
+            this.pictureBoxMain.MouseUp += new System.Windows.Forms.MouseEventHandler(this.PictureBoxMain_MouseUp);
+            // 
+            // buttonEllipse
+            // 
+            this.buttonEllipse.Location = new System.Drawing.Point(319, 12);
+            this.buttonEllipse.Name = "buttonEllipse";
+            this.buttonEllipse.Size = new System.Drawing.Size(75, 31);
+            this.buttonEllipse.TabIndex = 8;
+            this.buttonEllipse.Text = "Ellipse";
+            this.buttonEllipse.UseVisualStyleBackColor = true;
+            this.buttonEllipse.Click += new System.EventHandler(this.ButtonEllipse_Click);
+            // 
+            // buttonRectangle
+            // 
+            this.buttonRectangle.Location = new System.Drawing.Point(401, 12);
+            this.buttonRectangle.Name = "buttonRectangle";
+            this.buttonRectangle.Size = new System.Drawing.Size(98, 31);
+            this.buttonRectangle.TabIndex = 9;
+            this.buttonRectangle.Text = "Rectangle";
+            this.buttonRectangle.UseVisualStyleBackColor = true;
+            this.buttonRectangle.Click += new System.EventHandler(this.ButtonRectangle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonRectangle);
+            this.Controls.Add(this.buttonEllipse);
+            this.Controls.Add(this.pictureBoxMain);
+            this.Controls.Add(this.buttonTest);
+            this.Controls.Add(this.buttonColor);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonCircle);
+            this.Controls.Add(this.buttonLine);
+            this.Controls.Add(this.buttonDot);
+            this.Controls.Add(this.listBoxShapes);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMain)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxShapes;
+        private System.Windows.Forms.Button buttonDot;
+        private System.Windows.Forms.Button buttonLine;
+        private System.Windows.Forms.Button buttonCircle;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonColor;
+        private System.Windows.Forms.ColorDialog colorDialog;
+        private System.Windows.Forms.Button buttonTest;
+        private System.Windows.Forms.PictureBox pictureBoxMain;
+        private System.Windows.Forms.Button buttonEllipse;
+        private System.Windows.Forms.Button buttonRectangle;
+    }
+}
+
---
+++ ISMPaint/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ShapesLIb;
+
+namespace ISMPaint
+{
+    public partial class Form1 : Form
+    {
+        protected List<Shape> Shapes;
+        protected Mode Mode;
+        protected int MouseX, MouseY, MouseX2, MouseY2;
+        protected void AddShape(Shape shape)
+        {
+            Shapes.Add(shape);
+            listBoxShapes.Items.Add(shape);
+            pictureBoxMain.Refresh();
+        }
+        public void DeleteShape(int number)
+        {
+            Shapes.RemoveAt(number);
+            listBoxShapes.Items.RemoveAt(number);
+            pictureBoxMain.Refresh();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonColor_Click(object sender, EventArgs e)
+        {
+            if (colorDialog.ShowDialog() == DialogResult.OK)
+            {
+                buttonColor.BackColor = colorDialog.Color;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            buttonColor.BackColor = Color.Black;
+            Shapes = new List<Shape>();
+        }
+
+        private void ButtonTest_Click(object sender, EventArgs e)
+        {
+            AddShape(new Dot(buttonColor.BackColor, 40, 50));
+        }
+
+        private void ButtonDelete_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < listBoxShapes.Items.Count; i++)
+            {
+                if (listBoxShapes.GetSelected(i))
+                {
+                    DeleteShape(i);
+                    i--;
+                }
+            }
+            if(listBoxShapes.Items.Count > 0)
+            {
+                listBoxShapes.SetSelected(0, true);
+            }
+        }
+
+        private void PictureBoxMain_Paint(object sender, PaintEventArgs e)
+        {
+            for(int i = 0; i < Shapes.Count; i++)
+            {
+                Shapes[i].Draw(e.Graphics);
+            }
+        }
+
+
+
+        private void ButtonDot_Click(object sender, EventArgs e)
+        {
+            Mode = Mode.DrawDot;
+
+        }
+
+        private void ButtonLine_Click(object sender, EventArgs e)
+        {
+            Mode = Mode.DrawLine;
+        }
+
+        private void ButtonCircle_Click(object sender, EventArgs e)
+        {
+            Mode = Mode.DrawCircle;
+        }
+
+        private void ButtonEllipse_Click(object sender, EventArgs e)
+        {
+            Mode = Mode.DrawEllipse;
+        }
+
+        private void ButtonRectangle_Click(object sender, EventArgs e)
+        {
+            Mode = Mode.DrawRectangle;
+        }
+
+        private void PictureBoxMain_MouseDown(object sender, MouseEventArgs e)
+        {
+            switch (Mode) {
+                case Mode.DrawDot:
+                    Dot dot = new Dot(buttonColor.BackColor,
+                                      e.X, 
+                                      e.Y);
+                    AddShape(dot);
+                    break;
+                case Mode.DrawLine:
+                    MouseX = e.X;
+                    MouseY = e.Y;
+                    break;
+                case Mode.DrawCircle:
+                    MouseX = e.X;
+                    MouseY = e.Y;
+                    break;
+                case Mode.DrawEllipse:
+                    MouseX = e.X;
+                    MouseY = e.Y;
+                    break;
+                case Mode.DrawRectangle:
+                    MouseX = e.X;
+                    MouseY = e.Y;
+                    break;
+            }          
+            pictureBoxMain.Refresh();
+        }
+
+        private void PictureBoxMain_MouseMove(object sender, MouseEventArgs e)
+        {
+            Graphics graphics = pictureBoxMain.CreateGraphics();
+            switch (Mode)
+            {
+                
+                case Mode.DrawLine:
+                    if (e.Button == MouseButtons.Left)
+                    {
+                        pictureBoxMain.Refresh();
+                        MouseX2 = e.X;
+                        MouseY2 = e.Y;
+                        //AddShape(new Line(buttonColor.BackColor, MouseX, MouseY, e.X, e.Y));
+                        graphics.DrawLine(new Pen(buttonColor.BackColor, 1), MouseX, MouseY, e.X, e.Y);
+                    }
+                    break;
+                case Mode.DrawCircle:
+                    if (e.Button == MouseButtons.Left)
+                    {
+                        pictureBoxMain.Refresh();
+                        MouseX2 = e.X;
+                        MouseY2 = e.Y;
+                        int radius = Math.Abs(MouseX - MouseX2) + Math.Abs(MouseY - MouseY2);
+                        graphics.DrawEllipse(new Pen(Color.Black, 1), MouseX - radius / 2, MouseY - radius / 2, radius, radius);
+                    }
+                    break;
+                case Mode.DrawEllipse:
+                    if (e.Button == MouseButtons.Left)
+                    {
+                        pictureBoxMain.Refresh();
+                        MouseX2 = e.X;
+                        MouseY2 = e.Y;                      
+                        graphics.DrawEllipse(new Pen(buttonColor.BackColor, 1), MouseX, MouseY, MouseX2 - MouseX, MouseY2 - MouseY);
+                    }
+                    break;
+                case Mode.DrawRectangle:
+                    if (e.Button == MouseButtons.Left)
+                    {
+                        pictureBoxMain.Refresh();
+                        MouseX2 = e.X;
+                        MouseY2 = e.Y;
+                        if (MouseX > MouseX2 && MouseY > MouseY2)
+                        {
+                         graphics.DrawRectangle(new Pen(buttonColor.BackColor, 1), MouseX, MouseY, MouseX - MouseX2, MouseY - MouseY2);
+                        }
+                        else if (MouseX < MouseX2 && MouseY < MouseY2)
+                        {
+                                graphics.DrawRectangle(new Pen(buttonColor.BackColor, 1), MouseX, MouseY, MouseX2 - MouseX, MouseY2 - MouseY);
+                        }
+                        else if (MouseX < MouseX2 && MouseY > MouseY2)
+                        {
+                            graphics.DrawRectangle(new Pen(buttonColor.BackColor, 1), MouseX, MouseY2, Math.Abs(MouseX2 - MouseX), Math.Abs(MouseY2 - MouseY));  
+                        }
+                        else if(MouseX > MouseX2 && MouseY < MouseY2)
+                        {
+                            graphics.DrawRectangle(new Pen(buttonColor.BackColor, 1), MouseX2, MouseY, Math.Abs(MouseX2 - MouseX), Math.Abs(MouseY2 - MouseY));
+                        }
+                        
+                           
+                    }
+                    break;
+            }
+        }
+
+        private void PictureBoxMain_MouseUp(object sender, MouseEventArgs e)
+        {
+            switch (Mode) {
+                case Mode.DrawLine:
+                    Shape shape1 = new Line(buttonColor.BackColor, MouseX, MouseY, e.X, e.Y);
+                    AddShape(shape1);
+                    break;
+                case Mode.DrawCircle:
+                    Shape shape2 = new Circle(buttonColor.BackColor, MouseX, MouseY, Math.Abs(MouseX - MouseX2) + Math.Abs(MouseY - MouseY2));
+                    AddShape(shape2);
+                    break;
+                case Mode.DrawEllipse:
+                    Shape shape3 = new Ellipse(buttonColor.BackColor, MouseX, MouseY, MouseX - MouseX2, MouseY - MouseY2);
+                    AddShape(shape3);
+                    break;
+                case Mode.DrawRectangle:
+                    if (MouseX > MouseX2 && MouseY > MouseY2)
+                    {
+                        Shape shape4 = new ShapesLIb.Rectangle(buttonColor.BackColor, MouseX, MouseY, MouseX - MouseX2, MouseY - MouseY2);
+                        AddShape(shape4);
+                    }
+                    else if(MouseX < MouseX2 && MouseY < MouseY2)
+                    {
+                        Shape shape4 = new ShapesLIb.Rectangle(buttonColor.BackColor, MouseX, MouseY, MouseX2 - MouseX, MouseY2 - MouseY);
+                        AddShape(shape4);
+                    }
+                    else if (MouseX > MouseX2 && MouseY < MouseY2)
+                    {
+                        Shape shape4 = new ShapesLIb.Rectangle(buttonColor.BackColor, MouseX2, MouseY, Math.Abs(MouseX2 - MouseX), Math.Abs(MouseY2 - MouseY));
+                        AddShape(shape4);
+                    }
+                    else if (MouseX < MouseX2 && MouseY > MouseY2)
+                    {
+                        Shape shape4 = new ShapesLIb.Rectangle(buttonColor.BackColor, MouseX, MouseY2, Math.Abs(MouseX2 - MouseX), Math.Abs(MouseY2 - MouseY));
+                        AddShape(shape4);
+                        
+                    } 
+                    break;
+            }
+
+        }
+    }
+}
